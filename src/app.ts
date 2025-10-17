709951225905132161d9fc91142c0dfbace3c7c2
Deozblack <omarjs097@gmail.com>
2025-10-16 23:57:18 -0600

    feat: set up Express server with error handling and environment configuration

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log('Hola Mundo!');
+import express, { type Request, type Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use((err: unknown, req: Request, res: Response) => {
+  if (err instanceof Error) {
+    console.error(err.stack);
+    return res.status(500).json({ message: err.message });
+  }
+  console.error(err);
+  return res.status(500).json({ message: 'Something went wrong' });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
