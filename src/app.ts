cf0275abf7b616cab15b74070ab66d31c127a5ef
Deozblack <omarjs097@gmail.com>
2025-10-17 23:28:55 -0600

    fix: add type annotation for Express instance and improve server start condition

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { type Request, type Response } from 'express';
+import express, { type Request, type Response, type Express } from 'express';
 import { UserRouter } from './modules/users/infrastructure/http/User.routes.js';
 import { TaskRouter } from './modules/tasks/infrastructure/http/Task.routes.js';
 import { AuthMiddleware } from './shared/infrastructure/middlewares/Auth.middleware.js';
@@ -6,7 +6,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const app = express();
+const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
@@ -25,6 +25,11 @@ app.use((err: unknown, req: Request, res: Response) => {
   return res.status(500).json({ message: 'Something went wrong' });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(3000, () => {
+    // eslint-disable-next-line no-console
+    console.log('Server is running on http://localhost:3000');
+  });
+}
+
+export { app };
